c954636eb4660840ac37fd253461f3f1c7c7a579
vjrajput1 <vjrajput1@myseneca.ca>
2024-07-06 16:49:37 -0400

    final push

--- server.js
+++
@@ -1,9 +0,0 @@
-var HTTP_PORT = process.env.PORT || 8080;
-var express = require("express");
-var app = express();
-// setup a 'route' to listen on the default url path
-app.get("/", (req, res) => {
-res.send("Hello World!");
-});
-// setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT, ()=>{console.log("server listening on port: " + HTTP_PORT)});
